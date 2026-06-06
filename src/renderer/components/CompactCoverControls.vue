be4fac2ddbe43146e81ce36dfa4b28ca8960686f
bysanhz <bysan3749@gmail.com>
2026-06-06 18:21:49 +0800

    Linux桌面歌词窗口

--- src/renderer/components/CompactCoverControls.vue
+++ src/renderer/components/CompactCoverControls.vue
@@ -1,33 +1,20 @@
 <template>
-  <div
-    class="compact-cover-controls"
-    @mouseenter="isHover = true"
-    @mouseleave="isHover = false"
-  >
-    <img
-      class="compact-cover"
-      :src="coverUrl"
-      alt="当前歌曲封面"
-      draggable="false"
-    />
+  <div class="compact-cover-controls" @mouseenter="isHover = true" @mouseleave="isHover = false">
+    <img class="compact-cover" :src="coverUrl" alt="当前歌曲封面" draggable="false" />
 
     <!-- ======== newADD start====== -->
     <button
-        class="compact-like-button"
-        :class="{ liked: isLiked }"
-        :title="isLiked ? '取消喜欢' : '加入喜欢'"
-        @click.stop="toggleLike"
-        >
-        <SvgIcon :icon-class="isLiked ? 'heart-solid' : 'heart'" />
+      class="compact-like-button"
+      :class="{ liked: isLiked }"
+      :title="isLiked ? '取消喜欢' : '加入喜欢'"
+      @click.stop="toggleLike"
+    >
+      <SvgIcon :icon-class="isLiked ? 'heart-solid' : 'heart'" />
     </button>
     <!-- =========== newADD end ======== -->
 
     <div v-show="isHover" class="compact-control-mask">
-      <button
-        class="compact-control-button"
-        title="上一首"
-        @click.stop="playPrev"
-      >
+      <button class="compact-control-button" title="上一首" @click.stop="playPrev">
         <SvgIcon icon-class="previous" />
       </button>
 
@@ -39,11 +26,7 @@
         <SvgIcon :icon-class="isPlaying ? 'pause' : 'play'" />
       </button>
 
-      <button
-        class="compact-control-button"
-        title="下一首"
-        @click.stop="playNext"
-      >
+      <button class="compact-control-button" title="下一首" @click.stop="playNext">
         <SvgIcon icon-class="next" />
       </button>
     </div>
@@ -54,8 +37,7 @@
 import { onBeforeUnmount, onMounted, ref } from 'vue'
 import SvgIcon from './SvgIcon.vue'
 
-const DEFAULT_COVER =
-  'https://p2.music.126.net/UeTuwE7pvjBpypWLudqukA==/3132508627578625.jpg'
+const DEFAULT_COVER = 'https://p2.music.126.net/UeTuwE7pvjBpypWLudqukA==/3132508627578625.jpg'
 
 const isHover = ref(false)
 const isPlaying = ref(false)
@@ -86,11 +68,7 @@ const updatePlayerSnapshot = () => {
     const track = player.currentTrack
     const album = track?.album ?? track?.al
 
-    coverUrl.value =
-      player.pic ||
-      album?.picUrl ||
-      track?.picUrl ||
-      DEFAULT_COVER
+    coverUrl.value = player.pic || album?.picUrl || track?.picUrl || DEFAULT_COVER
 
     isPlaying.value = Boolean(player.playing)
   } catch (error) {
@@ -218,12 +196,9 @@ onMounted(() => {
   // ======== newADD start======
   window.addEventListener('message', handleOsdStatusMessage)
   // =========== newADD end ========
-  window.mainApi?.on(
-    'update-osd-playing-status',
-    (_event: unknown, value: boolean) => {
-      isPlaying.value = value
-    }
-  )
+  window.mainApi?.on('update-osd-playing-status', (_event: unknown, value: boolean) => {
+    isPlaying.value = value
+  })
 })
 
 onBeforeUnmount(() => {
@@ -378,5 +353,4 @@ onBeforeUnmount(() => {
   z-index: 2;
 }
 /* =========== newADD end ======== */
-
 </style>
