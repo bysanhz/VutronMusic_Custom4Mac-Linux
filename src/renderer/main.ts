96bf251af8f153672e53e8c32677b520953bd055
bysanhz <144031718+bysanhz@users.noreply.github.com>
2026-07-20 15:12:14 +0800

    加载窄窗口响应式样式

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -7,6 +7,10 @@ import router from './router'
 import i18n from './plugins/i18n'
 import 'virtual:svg-icons-register'
 import './assets/css/global.scss'
+// ======== newADD start======
+// 主窗口在 768px 以下的响应式覆盖，必须晚于 global.scss 加载。
+import './assets/css/responsive-window.scss'
+// =========== newADD end ========
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import DOMPurify from 'dompurify'
 import { dailyTask } from './utils'
