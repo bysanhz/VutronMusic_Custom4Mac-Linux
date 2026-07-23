7cf36979b9bc08bfc124abfeed03614c6ca1f8c1
bysanhz <144031718+bysanhz@users.noreply.github.com>
2026-07-23 12:42:51 +0800

    接入桌面歌词基准标签细化

--- src/renderer/utils/smoothWindowScale.ts
+++ src/renderer/utils/smoothWindowScale.ts
@@ -1,16 +1,20 @@
 /* ======== newADD start====== */
 import router from '../router'
 import { initializeBaselineWindowScale } from './baselineWindowScale'
+import { initializeDesktopLyricScaleLabels } from './desktopLyricScaleLabels'
 import { initializeMiniOsdScaleSettings } from './miniOsdScaleSettings'
 import { initializeWindowScaleCalibration } from './windowScaleCalibration'
 
 export const initializeSmoothWindowScale = () => {
   const cleanupCalibration = initializeWindowScaleCalibration(router)
   const cleanupMiniOsdSettings = initializeMiniOsdScaleSettings(router)
+  const cleanupDesktopLyricLabels =
+    initializeDesktopLyricScaleLabels(router)
   const cleanupScale = initializeBaselineWindowScale()
 
   return () => {
     cleanupScale?.()
+    cleanupDesktopLyricLabels?.()
     cleanupMiniOsdSettings?.()
     cleanupCalibration?.()
   }
