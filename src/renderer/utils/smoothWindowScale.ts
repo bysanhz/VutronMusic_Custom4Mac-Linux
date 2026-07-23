578ba4c339fd07defcae59ba21690bcbef18454b
bysanhz <144031718+bysanhz@users.noreply.github.com>
2026-07-23 14:20:32 +0800

    接入桌面歌词颜色响应式布局

--- src/renderer/utils/smoothWindowScale.ts
+++ src/renderer/utils/smoothWindowScale.ts
@@ -3,6 +3,7 @@ import router from '../router'
 import { initializeBaselineWindowScale } from './baselineWindowScale'
 import { initializeDesktopLyricScaleLabels } from './desktopLyricScaleLabels'
 import { initializeMiniOsdScaleSettings } from './miniOsdScaleSettings'
+import { initializeOsdColorSettingsLayout } from './osdColorSettingsLayout'
 import { initializeWindowScaleCalibration } from './windowScaleCalibration'
 import { initializeWindowScaleInlineActions } from './windowScaleInlineActions'
 
@@ -10,6 +11,7 @@ export const initializeSmoothWindowScale = () => {
   const cleanupCalibration = initializeWindowScaleCalibration(router)
   const cleanupMiniOsdSettings = initializeMiniOsdScaleSettings(router)
   const cleanupInlineActions = initializeWindowScaleInlineActions()
+  const cleanupColorLayout = initializeOsdColorSettingsLayout()
   const cleanupDesktopLyricLabels =
     initializeDesktopLyricScaleLabels(router)
   const cleanupScale = initializeBaselineWindowScale()
@@ -17,6 +19,7 @@ export const initializeSmoothWindowScale = () => {
   return () => {
     cleanupScale?.()
     cleanupDesktopLyricLabels?.()
+    cleanupColorLayout?.()
     cleanupInlineActions?.()
     cleanupMiniOsdSettings?.()
     cleanupCalibration?.()
