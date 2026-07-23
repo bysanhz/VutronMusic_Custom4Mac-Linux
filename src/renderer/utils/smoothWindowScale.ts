4a6b9815545b444e7e53eeea0209675fe037f63d
bysanhz <144031718+bysanhz@users.noreply.github.com>
2026-07-23 11:52:34 +0800

    在主窗口缩放入口接入确认式校准

--- src/renderer/utils/smoothWindowScale.ts
+++ src/renderer/utils/smoothWindowScale.ts
@@ -1,5 +1,15 @@
 /* ======== newADD start====== */
-export {
-  initializeBaselineWindowScale as initializeSmoothWindowScale
-} from './baselineWindowScale'
+import router from '../router'
+import { initializeBaselineWindowScale } from './baselineWindowScale'
+import { initializeWindowScaleCalibration } from './windowScaleCalibration'
+
+export const initializeSmoothWindowScale = () => {
+  const cleanupScale = initializeBaselineWindowScale()
+  const cleanupCalibration = initializeWindowScaleCalibration(router)
+
+  return () => {
+    cleanupCalibration?.()
+    cleanupScale?.()
+  }
+}
 /* =========== newADD end ======== */
