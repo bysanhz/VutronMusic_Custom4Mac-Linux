fa5a297db592b780f7d2150ef66521da76f8bae5
bysanhz <144031718+bysanhz@users.noreply.github.com>
2026-07-23 12:39:42 +0800

    接入迷你桌面歌词独立基准设置

--- src/renderer/utils/smoothWindowScale.ts
+++ src/renderer/utils/smoothWindowScale.ts
@@ -1,14 +1,17 @@
 /* ======== newADD start====== */
 import router from '../router'
 import { initializeBaselineWindowScale } from './baselineWindowScale'
+import { initializeMiniOsdScaleSettings } from './miniOsdScaleSettings'
 import { initializeWindowScaleCalibration } from './windowScaleCalibration'
 
 export const initializeSmoothWindowScale = () => {
   const cleanupCalibration = initializeWindowScaleCalibration(router)
+  const cleanupMiniOsdSettings = initializeMiniOsdScaleSettings(router)
   const cleanupScale = initializeBaselineWindowScale()
 
   return () => {
     cleanupScale?.()
+    cleanupMiniOsdSettings?.()
     cleanupCalibration?.()
   }
 }
