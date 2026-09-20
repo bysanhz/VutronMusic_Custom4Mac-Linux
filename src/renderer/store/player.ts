f5a8fdf72f18f0553ea092b537a6b1b821013fcf
bysanhz <144031718+bysanhz@users.noreply.github.com>
2026-09-20 11:42:34 +0800

    refactor: keep pending duration accounting in scrobble API

--- src/renderer/store/player.ts
+++ src/renderer/store/player.ts
@@ -30,7 +30,6 @@ import {
   registerSleepTimerPauseHandler
 } from '../utils/sleepTimerSettings'
 import { markPlaybackEndReason } from '../utils/playbackFeedback'
-import { addPendingNeteaseListenSeconds } from '../utils/neteaseListenPending'
 import { Track, serviceName, lyricLine } from '@/types/music'
 
 interface biquadType {
@@ -1020,10 +1019,6 @@ export const usePlayerStore = defineStore(
             result
           })
           return
-        }
-
-        if (!result?.skipped && !result?.deduplicated) {
-          addPendingNeteaseListenSeconds(listenedSeconds)
         }
 
         window.dispatchEvent(
