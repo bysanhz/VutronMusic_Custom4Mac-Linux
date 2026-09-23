abc0ef2c1e41693e7c25ebf0aac79e179865f79b
bysanhz <144031718+bysanhz@users.noreply.github.com>
2026-09-23 15:24:25 +0800

    style: rebalance lyric preset panes to 60-40

--- src/renderer/utils/v327FeatureShared.ts
+++ src/renderer/utils/v327FeatureShared.ts
@@ -182,7 +182,7 @@ export const injectV327FeatureStyle = (): void => {
       width: 100%;
       min-width: 0;
       display: grid;
-      grid-template-columns: minmax(0, 1.55fr) minmax(0, 0.45fr);
+      grid-template-columns: minmax(0, 3fr) minmax(0, 2fr);
       align-items: stretch;
       gap: 16px;
       box-sizing: border-box;
